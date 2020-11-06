bb95c69ece0a2029859d712cede83b127939ddf7
lhaeussler <lhaeussler@onbtineuss.de>
2020-11-06 16:36:57 +0100

    Introduced Menu.vue

--- web_frontend/src/router/index.js
+++ web_frontend/src/router/index.js
@@ -1,15 +1,20 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
+import Router from 'vue-router'
+import menu from '@/components/menu'
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'menu',
+      component: menu
     }
   ]
 })
